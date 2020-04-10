1fecc46ac77f45689332b76ce028292f528c6fe7
David Johnson <mcfarlander@charter.net>
2020-04-10 09:08:45 -0500

    add documentation for reports and search results

--- src/models/StoreInventoryReport.ts
+++ src/models/StoreInventoryReport.ts
@@ -1,6 +1,14 @@
 import { Store } from "./Store"
 import { ProductInventoryReport } from "./ProductInventoryReport"
 
+
+/**
+ * StoreInventoryReport.ts
+ * The Store Inventory Report represents a [[Store]] and all of the [[ProductInventoryReport]] associated with it.
+ * 
+ * Created by S Perkins on 2020-04-01
+ * 
+ */
 export class StoreInventoryReport {
     store: Store
     products: ProductInventoryReport[]
